0a0415037022bd46ef843bb2a3d6e4ebfd61ac61
Emil Ifraimov <emilifraim@gmail.com>
2019-06-24 15:58:21 -0400

    Updates

---
+++ precache-manifest.9d093e0bce082f1ef18425faf1fc7bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd249c439fe3d342927908a49e85917a",
+    "url": "/allocation-strategy/index.html"
+  },
+  {
+    "revision": "ae96a6946192ac3859e4",
+    "url": "/allocation-strategy/static/css/main.5f56e3e4.chunk.css"
+  },
+  {
+    "revision": "78fbaf2ed77398d1d68a",
+    "url": "/allocation-strategy/static/js/2.b84443a4.chunk.js"
+  },
+  {
+    "revision": "ae96a6946192ac3859e4",
+    "url": "/allocation-strategy/static/js/main.92ce2d04.chunk.js"
+  },
+  {
+    "revision": "f47fafe2af334aa42b6f",
+    "url": "/allocation-strategy/static/js/runtime~main.5a5c6f1d.js"
+  }
+]);
